35e3fbc06b7e2bec9678e3baed83d7cd6541d0ba
Yonaz <jonas_ziegler@outlook.com>
2024-03-09 23:52:18 +0100

    folien

--- auswertung_folien.py
+++ auswertung_folien.py
@@ -13,4 +13,27 @@ print("durchschnitt jahr:", grouped_year_df.mean())
 for station, group in combined_tage_df.groupby('zaehlstelle'):
     
     print(station, group['gesamt'].sum(), group['gesamt'].mean())
-    
+
+
+
+grouped_year_df = combined_tage_df.groupby(combined_tage_df['datum'].dt.year)['gesamt'].sum()
+
+combined_tage_df = combined_tage_df[combined_tage_df['zaehlstelle'] != 'Kreuther']
+combined_tage_df = combined_tage_df[combined_tage_df['zaehlstelle'] != 'Arnulf']
+
+
+# Filtern der Daten für 2012 und 2022
+data_2012 = combined_tage_df[combined_tage_df['datum'].dt.year == 2012]
+data_2012 = data_2012.groupby(data_2012['datum'])
+data_2022 = combined_tage_df[combined_tage_df['datum'].dt.year == 2022]
+data_2022 = data_2022.groupby(data_2022['datum'])
+
+# Berechnung der durchschnittlichen täglichen Radverkehrszahlen für beide Jahre
+avg_daily_traffic_2012 = data_2012['gesamt'].sum().mean()
+avg_daily_traffic_2022 = data_2022['gesamt'].sum().mean()
+
+avg_daily_traffic_2012 = avg_daily_traffic_2012/4
+avg_daily_traffic_2022 = avg_daily_traffic_2022/4
+
+# Ausgabe der durchschnittlichen täglichen Radverkehrszahlen
+print(avg_daily_traffic_2012, avg_daily_traffic_2022)
